705f183c1374833c36bee1f052d06ddd83adf4ef
sungwoo1548 <swo1548@gmail.com>
2020-02-02 21:33:35 +0900

    test

--- opencv4nodejs/routers/index.js
+++ opencv4nodejs/routers/index.js
@@ -7,7 +7,13 @@ router.get("/", (req, res, next) => {
     cv.imreadAsync("public/img/monalisa.png", (err, mat) => {
         if (err) console.log(err);
         console.log(mat);
-    })
+
+        // cv.imshow("test", mat);
+        cv.imshow('a window name', mat);
+        cv.waitKey();
+        console.log(__dirname);
+    });
+
     res.json({ msg: "connection test" });
     next();
 });
